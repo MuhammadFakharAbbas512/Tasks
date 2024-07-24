cb5094574ca29bed69751aed8d3888cfbd71cdb5
Muhammad Fakhar Abbas <i210448@nu.edu.pk>
2024-07-24 17:26:35 +0500

    Improved Chat implemented using HTTP and Express

---
+++ chat/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = 3000;
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    io.emit('message', msg); // Broadcast message to all connected clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+// Root route handler
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'chat.html'));
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
